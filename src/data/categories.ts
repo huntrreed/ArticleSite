8621693f850d158c43f3b108a8500f4e2deb180b
Hunter <hunterreed@mac.lan>
2025-03-05 10:19:31 -0700

    category

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = ['Skiing', 'Snowboarding', 'Camping', 'Hiking', 'Category 5'] as const
+export const CATEGORIES = ['Skiing', 'Snowboarding', 'Camping', 'Hiking', 'Reviews'] as const
